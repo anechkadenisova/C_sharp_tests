93b9cae488f6a3b22b83f6354f1da1d78cd11678
Anna Denisova <anechka.k93@gmail.com>
2023-04-02 00:42:46 +0500

    Added the first version calculator

---
+++ home_2/home_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+            // Test2();
+            // Test3();
+            // Test4();
+        }
+
+        public static void Test1()
+        {
+            char operation;
+            double result;
+            Console.Write("Enter the first number: ");
+            double operand1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            double operand2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the arithmetic operation (+,-,*,/): ");
+            operation = (char)Console.Read();
+
+            switch (operation)
+            {
+                case '+':
+                    result = operand1 + operand2;
+                    Console.WriteLine("Result = " + result);
+                    break;
+                case '-':
+                    result = operand1 - operand2;
+                    Console.WriteLine("Result = " + result);
+                    break;
+                case '*':
+                    result = operand1 * operand2;
+                    Console.WriteLine("Result = " + result);
+                    break;
+                case '/':
+                    result = operand1 / operand2;
+                    Console.WriteLine("Result = " + result);
+                    break;
+                default:
+                    Console.WriteLine("hh");
+                    break;
+            }
+        }
+    }
+}
